c8ae7fcb7eb61640d916bf7ebd2e61cf925642d4
Tobias Kiertscher <kiertscher@th-brandenburg.de>
2017-11-15 11:55:43 +0100

    completed first implementation

--- HtmlDisplay/MainWindow.xaml.cs
+++ HtmlDisplay/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 
 namespace Mastersign.HtmlDisplay
@@ -21,9 +19,72 @@ namespace Mastersign.HtmlDisplay
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string FilePath { get; }
+
+        private bool ValidFile => FilePath != null && File.Exists(FilePath);
+
+        private Uri Url => ValidFile ? new Uri($"file:///{FilePath}") : null;
+
+        private FileSystemWatcher Watcher { get; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            var argv = Environment.GetCommandLineArgs();
+            var filePath = argv.Length == 2 ? argv[1] : null;
+            FilePath = Path.IsPathRooted(filePath) ? filePath : Path.Combine(Environment.CurrentDirectory, filePath);
+
+            if (FilePath != null)
+            {
+                try
+                {
+                    Watcher = new FileSystemWatcher(
+                        Path.GetDirectoryName(FilePath),
+                        Path.GetFileName(FilePath));
+                    Watcher.Changed += FileChangedHandler;
+                    Watcher.Deleted += FileDeletedHandler;
+                    Watcher.Created += FileCreatedHandler;
+                    Watcher.EnableRaisingEvents = true;
+                }
+                catch (ArgumentException)
+                {
+                    MessageBox.Show(this, $"Failed to watch the given path: {FilePath}", Title, 
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void Display(Uri url) => webBrowser.Navigate(url ?? new Uri("about:blank"));
+
+        private void UpdateDisplay() => Display(Url);
+
+        private void LoadedHandler(object sender, RoutedEventArgs e)
+        {
+            UpdateDisplay();
+        }
+
+        private void FileCreatedHandler(object sender, FileSystemEventArgs e)
+        {
+            Debug.WriteLine("FileSystemWatcher: File Created");
+            Dispatcher.BeginInvoke((Action)UpdateDisplay);
+        }
+
+        private void FileChangedHandler(object sender, FileSystemEventArgs e)
+        {
+            Debug.WriteLine("FileSystemWatcher: File Changed");
+            Dispatcher.BeginInvoke((Action<bool>)webBrowser.Refresh, false);
+        }
+
+        private void FileDeletedHandler(object sender, FileSystemEventArgs e)
+        {
+            Debug.WriteLine("FileSystemWatcher: File Deleted");
+            // UpdateDisplay();
+        }
+
+        private void WebBrowserLoadCompletedHandler(object sender, NavigationEventArgs e)
+        {
+            Title = ((dynamic)webBrowser.Document).Title;
         }
     }
 }
